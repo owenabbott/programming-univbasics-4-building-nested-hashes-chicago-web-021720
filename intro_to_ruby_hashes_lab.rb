144cb54ec317fc890a877bee3333c226b0cb132d
Owen Abbott <ocabbott@gmail.com>
2020-01-28 20:31:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ end
 
 def monopoly_with_third_tier
   	hash = {:railroads => {
-  	  :pieces => 4, :rent_in_dollars => "passed"}
+  	  :pieces => 4, :rent_in_dollars => {:one=>1, :two=>2, :three=>3, :four=>4}}
 	}
 # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
